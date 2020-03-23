bc74b957af66ae39363b1d4c16171e827beb5b64
Igor Popovich <popovichia@gmail.com>
2020-03-23 09:21:11 +0300

    Commit all 2020-03-23 09-22

---
+++ exercises/src/main/java/javacore/exercises/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacore.exercises;
+
+/**
+ *
+ * @author igor
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(0x00));
+        System.out.println(Integer.toBinaryString(0x66));
+        System.out.println(Integer.toBinaryString(0xFF));
+        System.out.println(Integer.toBinaryString(0xFF));
+        System.out.println(Integer.toBinaryString(0xFF));
+        System.out.println(Integer.toBinaryString(0x7E));
+        System.out.println(Integer.toBinaryString(0x3C));
+        System.out.println(Integer.toBinaryString(0x18));
+    }
+}
