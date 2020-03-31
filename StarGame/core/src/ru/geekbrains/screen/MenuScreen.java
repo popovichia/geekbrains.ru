a43ce22059010f90ea9546bb0ab4f10da84f3368
Igor Popovich <popovichia@gmail.com>
2020-03-31 09:04:29 +0300

    Master commit 2020-03-31 09-05

--- StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
+++ StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
@@ -3,27 +3,30 @@ package ru.geekbrains.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.exception.GameException;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprites.Background;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture img;
-    private Vector2 posCurrent;
-    private Vector2 posEnd;
-    private Vector2 posDelta;
-    private Vector2 v;
+    private Texture bg;
+    private Background background;
+    private Vector2 pos;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        v = new Vector2(1.0f, 1.0f);
-        posCurrent = new Vector2();
-        posEnd = new Vector2();
-        posDelta = new Vector2();
+        bg = new Texture("textures/bg.png");
+        try {
+            background = new Background(bg);
+        } catch (GameException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        pos = new Vector2();
     }
 
     @Override
@@ -35,32 +38,29 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        bg.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        posEnd.set(screenX, Gdx.graphics.getHeight() - screenY);
-        posDelta = posEnd.cpy().sub(posCurrent);
-        v.set(posDelta.x/posDelta.len(), posDelta.y/posDelta.len());
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        pos.set(touch);
         return false;
     }
 
     private void update(float delta) {
-        if (posEnd.cpy().sub(posCurrent).len() >= 0.5) {
-            posCurrent.add(v);
-        } else {
-            posCurrent.x = posEnd.x;
-            posCurrent.y = posEnd.y;
-        }
     }
 
     private void draw() {
         Gdx.gl.glClearColor(0.5f, 0.7f, 0.8f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, posCurrent.x, posCurrent.y);
+        background.draw(batch);
         batch.end();
     }
 
